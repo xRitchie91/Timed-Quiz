2e249907cb3c5aa52f11c8689deaad9ef6ad272f
xRitchie91 <74946954+xRitchie91@users.noreply.github.com>
2021-03-15 02:57:33 -0500

    Recreated Repository
    Repository recreated after repository had to be manually updated outside of git bash.

---
+++ questions.js
@@ -0,0 +1,27 @@
+var jsQuestions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "A very useful tool used during development and debugging for printing content to the debugger is?",
+    choices: ["Nah fam", "Blue Steel", "Pizza", "for loops"],
+    answer: "for loops"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ___?",
+    choices: ["Other Arrays", "Booleans", "Numbers and Strings", "All of the Above"],
+    answer: "All of the Above"
+  },
+  {
+    title: "String values must be enclosed with ___ when being assigned to variables?",
+    choices: ["Curly Brackets", "Bevo", "Threads", "Carne Asada Fries"],
+    answer: "Curly Brackets"
+  }
+];
